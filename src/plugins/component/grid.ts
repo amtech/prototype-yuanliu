87178f0de854c6748d0e06bfc81ec7c53f7fff9f
TYW <taoyongwen@live.com>
2022-08-24 21:45:52 +0800

    hot

--- src/plugins/component/grid.ts
+++ src/plugins/component/grid.ts
@@ -7,10 +7,45 @@ import { IComponent } from "../../common/interfaceDefine"
     onPreview: () => {
         var gird = document.createElement("div");
         return gird;
-    }, onRender: () => {
+    }, onRender: (component:IComponent) => {
         var gird = document.createElement("div");
+
+        if(component.property!=undefined&&component.property.isHost!=undefined&&component.property.isHost.context=="true"){
+
+            gird.onmouseover=()=>{
+                gird.style.backgroundColor="rgba(175,175,175,0.2)";
+                gird.style.cursor="pointer";
+
+            };
+            gird.onmouseleave=()=>{
+
+                gird.style.cssText=component.style;
+            }
+            gird.onclick = () => {
+                if (component.blue.event.click.on != undefined) {
+                    component.blue.event.click.on();
+                }
+    
+            }
+    
+
+       //     gird.setAttribute("data-hot",component.property.isHost.context);
+
+        }
+    
         return {root:gird,content:gird};
     },
+    property: {
+        isHost: { label: "热区", type: "bool", context: "false", },
+    
+    },
+    blue: {
+        event: {
+            click: {
+                label: "单击"
+            }
+        }
+    }
 }
 export default function load(){
     return component;
